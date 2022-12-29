4234dc391ba8516483f509aa97af06214c7bf203
subru-37 <easwaransubramaniOO7@gmail.com>
2022-12-29 18:47:18 +0530

    form building

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Landing from './pages/landing/Landing';
 import Onboarding from './pages/Onboarding/Onboarding';
-import Addtank from './pages/Addtank/Addtank';
+import Addfarm from './pages/addfarm/Addfarm';
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Landing/>}></Route>
         <Route path="/onboarding" element={<Onboarding/>}></Route>
-        <Route path='/addTank' element={<Addtank/>}></Route>
+        <Route path='/addFarm' element={<Addfarm/>}></Route>
       </Routes>
     </div>
   )
--- FrontEnd/src/pages/Addtank/Addtank.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Addtank() {
-  return (
-    <div>Addtank</div>
-  )
-}
