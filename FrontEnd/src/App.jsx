198c9cd5da3f267c21a294b91b6c6a747abd214f
Aldrin Jenson <aldrinjenson@gmail.com>
2022-12-30 05:05:23 +0530

    readd auth components

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,37 +5,39 @@ import Landing from "./pages/landing/Landing";
 import Onboarding from "./pages/Onboarding/Onboarding";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Addfarm from "./pages/addfarm/Addfarm";
-import Marketplace from './pages/marketplace/Marketplace';
+import Marketplace from "./pages/marketplace/Marketplace";
 import Signup from "./pages/auth/signup";
 import SignIn from "./pages/auth/signin";
 import initialize from "../firebase";
 
-import Addtank from './pages/addtank/Addtank';
+import Addtank from "./pages/addtank/Addtank";
 export const ThemeContext = createContext();
 export default function App() {
   initialize();
   const [state, setState] = useState({
-    Name:'',
-    Bio: '',
-    Location: '',
-    tname:'',
-    fishes:'',
-    Email:'',
-    Password:'',
-    CPassword:''
-})
+    Name: "",
+    Bio: "",
+    Location: "",
+    tname: "",
+    fishes: "",
+    Email: "",
+    Password: "",
+    CPassword: "",
+  });
   return (
-    <ThemeContext.Provider value={{state,setState}}>
-    <div>
-      <Routes>
-        <Route path='/' element={<Landing/>}></Route>
-        <Route path="/onboarding" element={<Onboarding/>}></Route>
-        <Route path="/dashboard" element={<Dashboard/>}></Route>
-        <Route path='/addFarm' element={<Addfarm/>}></Route>
-        <Route path='/addTank' element={<Addtank/>}></Route>
-        <Route path='/marketplace' element={<Marketplace/>}></Route>
-      </Routes>
-    </div>
+    <ThemeContext.Provider value={{ state, setState }}>
+      <div>
+        <Routes>
+          <Route path='/signin' element={<SignIn />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/' element={<Landing />}></Route>
+          <Route path='/onboarding' element={<Onboarding />}></Route>
+          <Route path='/dashboard' element={<Dashboard />}></Route>
+          <Route path='/addFarm' element={<Addfarm />}></Route>
+          <Route path='/addTank' element={<Addtank />}></Route>
+          <Route path='/marketplace' element={<Marketplace />}></Route>
+        </Routes>
+      </div>
     </ThemeContext.Provider>
   );
 }
--- FrontEnd/src/pages/auth/signup.jsx
+++ FrontEnd/src/pages/auth/signup.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from "react";
-import { signUpWithEmail } from "../../../firebase";
+import { CssVarsProvider } from "@mui/joy/styles";
+import Sheet from "@mui/joy/Sheet";
+import Typography from "@mui/joy/Typography";
+import TextField from "@mui/joy/TextField";
+import Button from "@mui/joy/Button";
+import Link from "@mui/joy/Link";
+import { useState } from "react";
 
 export default function signin() {
-  const handleSignup = () => {
-    if (password !== confirmPassword) {
-      alert("unmatched password");
-      return;
-    }
-    signUpWithEmail(email, password);
-  };
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   return (
     <CssVarsProvider>
       <main>
-        {/* <ModeToggle /> */}
         <Sheet
           sx={{
             width: 300,
@@ -48,6 +46,7 @@ export default function signin() {
             type='email'
             placeholder='johndoe@email.com'
             value={email}
+            onChange={(e) => setEmail(e.target.value)}
             label='Email'
           />
           <TextField
@@ -55,18 +54,18 @@ export default function signin() {
             type='password'
             value={password}
             placeholder='password'
+            onChange={(e) => setPassword(e.target.value)}
             label='Password'
           />
           <TextField
             name='confirm password'
-            value={confirmPassword}
             type='password'
             placeholder='confirm password'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             label='Confirm Password'
           />
-          <Button onClick={handleSignup} sx={{ mt: 1 }}>
-            Sign Up
-          </Button>
+          <Button sx={{ mt: 1 /* margin top */ }}>Sign Up</Button>
           <Typography
             endDecorator={<Link href='/signin'>Log In</Link>}
             fontSize='sm'
