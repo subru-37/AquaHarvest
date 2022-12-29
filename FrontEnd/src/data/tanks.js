061f72497c6e593f650a215efa0b461aff6acbd7
jithin-j <jithinjagadeesh1@gmail.com>
2022-12-30 03:22:23 +0530

    Added dashboard

---
+++ FrontEnd/src/data/tanks.js
@@ -0,0 +1,35 @@
+const data = [
+  {
+    id: 1,
+    name: "Tank-1",
+    location: "Ernakulam",
+    variety: ["Rohu", "Catla"],
+    pH: 3.8,
+    O2: 30,
+  },
+  {
+    id: 2,
+    name: "Tank-2",
+    location: "Kottayam",
+    variety: ["Rohu", "Catla"],
+    pH: 4.8,
+    O2: 40,
+  },
+  {
+    id: 3,
+    name: "Tank-3",
+    location: "Thrissur",
+    variety: ["Rohu", "Catla"],
+    pH: 3.9,
+    O2: 30,
+  },
+  {
+    id: 4,
+    name: "Tank-4",
+    location: "Thrissur",
+    variety: ["Rohu", "Catla"],
+    pH: 3.9,
+    O2: 30,
+  },
+];
+export default data;
