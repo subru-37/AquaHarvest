2f99878e1e7a2c286c61c4810a9f70259f0bae30
subru-37 <easwaransubramaniOO7@gmail.com>
2022-12-29 17:16:42 +0530

    minor changes 1

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Landing from './pages/landing/Landing';
 import Onboarding from './pages/Onboarding/Onboarding';
+import Addtank from './pages/Addtank/Addtank';
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Landing/>}></Route>
         <Route path="/onboarding" element={<Onboarding/>}></Route>
+        <Route path='/addTank' element={<Addtank/>}></Route>
       </Routes>
     </div>
   )
