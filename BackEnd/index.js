e73091e751e0e7bdf8f5e664ddc4d1a684bf39c6
hannasalam <hannasalam113@gmail.com>
2022-12-30 04:32:12 +0530

    mailing feature done

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const TankController = require("./controllers/tankController");
 
 mongoose.connect(
     'mongodb+srv://hanna:passwordsss@cluster0.0b10im5.mongodb.net/?retryWrites=true&w=majority', {
@@ -10,6 +11,8 @@ mongoose.connect(
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+TankController(app);
 
 
 app.listen(process.env.PORT||3000);
