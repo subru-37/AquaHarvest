4290d46b35cad6e356601282b801caa27f6c57c1
jithin-j <jithinjagadeesh1@gmail.com>
2022-12-29 16:54:12 +0530

    Onboarding initial

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Landing from './pages/landing/Landing';
+import Onboarding from './pages/Onboarding/Onboarding';
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Landing/>}></Route>
+        <Route path="/onboarding" element={<Onboarding/>}></Route>
       </Routes>
     </div>
   )
---
+++ FrontEnd/src/pages/Onboarding/Onboarding.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./onboarding.css"
+
+const Onboarding = () => {
+  return (
+    <div className='onb-container'>
+        <div className='firstPara'></div>
+        <Link to="/addTank"><button>Add Existing Tank</button></Link>
+        <a href="https://en.wikipedia.org/wiki/Fish_farming">New to fish farming? Check out details here</a>
+    </div>
+  )
+}
+
+export default Onboarding
