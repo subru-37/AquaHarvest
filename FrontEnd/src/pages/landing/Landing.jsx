a17cbf20e4064181ed78e951395aaf1fd70131c6
subru-37 <easwaransubramaniOO7@gmail.com>
2022-12-29 16:36:00 +0530

    Landing page customization

--- FrontEnd/src/pages/landing/Landing.jsx
+++ FrontEnd/src/pages/landing/Landing.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import LandingImg from "../../images/background.png"
-import "./landing.css"
+import "./landing.css";
+import { Link } from 'react-router-dom';
 
 export default function Landing() {
   return (
-    <div className='container'>Landing</div>
+    <div className='container'>
+        <h2 className='h2'>
+            Aqua Harvest
+        </h2>
+        <div className='section2'>
+            <button className='.button'>
+                <Link style={{textDecoration: 'none'}}><h3 className='h3'>Getting Started</h3></Link>
+            </button>
+        </div>
+    </div>
   )
 }
