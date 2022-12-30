e35b0596b3287ed15e26e79e348a414e4007e441
hannasalam <hannasalam113@gmail.com>
2022-12-30 11:21:37 +0530

    error resolved

--- BackEnd/controllers/fishController.js
+++ BackEnd/controllers/fishController.js
@@ -1,28 +1,9 @@
 const Tank = require('../models/Tank');
 module.exports = function(app){
     app.get('/getProgress', async (req, res) => {
-        res.setHeader('Access-Control-Allow-Credentials', true)
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        // another common pattern
-        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        res.setHeader(
-          'Access-Control-Allow-Headers',
-          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        )
-        var fishDetails = {
-            Mrigal: 365,
-            Catla: 365,
-            Catfish: 243
-        }
-        var currentDate = new Date();
-        console.log(currentDate);
-        var startDate = new Date("12/30/2022");
-        var days =  parseInt((currentDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));
-        console.log(days);
         try{
-            var res = Tank.find({tankName: req.query.tankName});
-            res.json(days);
+            var resp = await Tank.find({tankName: req.query.tankName});
+            res.json(resp);
         }catch(err){
             res.status(500).json(err);
         }
